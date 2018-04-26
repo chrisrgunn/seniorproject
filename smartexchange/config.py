85a60fcefb432021d29ebba8d98588758946eec6
chrisrgunn <chrisrgunn@gmail.com>
2018-04-26 12:22:18 -0700

    added ability to switch between databases

--- smartexchange/config.py
+++ smartexchange/config.py
@@ -1,6 +1,6 @@
-
+import os
 # You need to have a database called smartexchange inside your local MongoDB
 MONGOALCHEMY_DATABASE = 'smartexchange'
-MONGOALCHEMY_CONNECTION_STRING = 'mongodb://localhost:27017/smartexchange'
+MONGOALCHEMY_CONNECTION_STRING = os.environ.get('DATABASE_URL', 'mongodb://localhost:27017/smartexchange')
 
 SALT = b'$2b$12$WDSY0pVdH/OP7RuzGJouBe'
